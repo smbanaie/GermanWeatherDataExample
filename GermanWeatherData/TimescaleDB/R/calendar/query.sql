9db11a5568e45afac52abe477ca9c6a6dc94c821
Philipp Wagner <pwag86@googlemail.com>
2018-12-28 09:23:10 +0100

    TimescaleDB: Fix Query. Return date_trunc as date-type

--- GermanWeatherData/TimescaleDB/R/calendar/query.sql
+++ GermanWeatherData/TimescaleDB/R/calendar/query.sql
@@ -1,4 +1,4 @@
-SELECT s.identifier "station", date_trunc('day', w.timestamp) "day", avg(w.air_temperature_at_2m) "temperature"
+SELECT s.identifier "station", date_trunc('day', w.timestamp)::date "day", avg(w.air_temperature_at_2m) "temperature"
 FROM sample.weather_data w
     INNER JOIN sample.station s on w.station_identifier = s.identifier
 WHERE s.identifier = '{{station}}' AND (w.timestamp >= '{{start_date}}'::date AND w.timestamp < '{{end_date}}'::date)
